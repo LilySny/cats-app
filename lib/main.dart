be320265e1aa8926d40ec0ac3020d5787a53d22a
LilySny <liviacastilholi@gmail.com>
2022-09-17 20:58:06 -0300

    chore: add firebase configuration for platforms

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:cats_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  
   runApp(const CatsApp());
 }
 

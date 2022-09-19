294664cf45cfbeee7958da7061e89736320ba39e
LilySny <liviacastilholi@gmail.com>
2022-09-19 00:53:39 -0300

    feat: changes in TextTheme - add Roboto fontFamily - fix properties TextStyle height

--- lib/src/shared/theme/themes/text_theme.dart
+++ lib/src/shared/theme/themes/text_theme.dart
@@ -1,46 +1,54 @@
 import 'package:cats_app/src/shared/theme/color_scheme.dart';
 import 'package:flutter/material.dart';
 
-final TextTheme buttonTheme = TextTheme(
-  headlineLarge: TextStyle(
+final _textThemeType = Typography.material2018().white;
+
+final TextTheme defaultTextTheme = _textThemeType.copyWith(
+  headline1: _textThemeType.headline1?.copyWith(
     color: catsAppColorScheme.secondary,
     fontSize: 34.0,
-    height: 36.0,
+    height: 1.06,
     fontWeight: FontWeight.bold,
+    fontFamily: 'Roboto',
   ),
-  bodyText1: TextStyle(
+  headline2: _textThemeType.bodyText1?.copyWith(
     color: catsAppColorScheme.secondary,
     fontSize: 16.0,
-    height: 24.0,
+    height: 1.5,
     fontWeight: FontWeight.bold,
     letterSpacing: 0.5,
+    fontFamily: 'Roboto',
   ),
-  bodySmall: TextStyle(
-    color: catsAppColorScheme.onSecondary,
-    fontSize: 12.0,
-    height: 16.0,
-    fontWeight: FontWeight.normal,
-    letterSpacing: 0.4,
-  ),
-  bodyMedium: TextStyle(
+  bodyText1: _textThemeType.bodyMedium?.copyWith(
     color: catsAppColorScheme.onSecondary,
     fontSize: 16.0,
-    height: 24.0,
+    height: 1.5,
     fontWeight: FontWeight.normal,
     letterSpacing: 0.5,
+    fontFamily: 'Roboto',
   ),
-  button: TextStyle(
+  bodyText2: _textThemeType.bodySmall?.copyWith(
+    color: catsAppColorScheme.onSecondary,
+    fontSize: 12.0,
+    height: 1.33,
+    fontWeight: FontWeight.normal,
+    letterSpacing: 0.4,
+    fontFamily: 'Roboto',
+  ),
+  button: _textThemeType.button?.copyWith(
     color: catsAppColorScheme.tertiary,
     fontSize: 14.0,
-    height: 16.0,
+    height: 1.14,
     fontWeight: FontWeight.w500,
     letterSpacing: 1.25,
+    fontFamily: 'Roboto',
   ),
-  caption: TextStyle(
+  caption: _textThemeType.caption?.copyWith(
     color: catsAppColorScheme.primary,
     fontSize: 14.0,
-    height: 16.0,
+    height: 1.33,
     fontWeight: FontWeight.w500,
     letterSpacing: 1.25,
+    fontFamily: 'Roboto',
   ),
 );
